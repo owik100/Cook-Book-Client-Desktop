978153ed67ca6076b77e2924949e589a77e5ae20
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2020-03-20 17:07:50 +0100

    Add Unit Test project to API

--- Program.cs
+++ Program.cs
@@ -10,11 +10,14 @@ namespace Cook_Book_Client_Desktop
 {
     class Program
     {
+        private static readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         [STAThread]
         static void Main()
         {
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            _logger.Info("Application started");
 
             App application = new App();
             application.InitializeComponent();
